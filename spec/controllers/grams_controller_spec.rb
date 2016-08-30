15002e902aff23e2b189b0725ee84328796c7754
Gonzalo <gonsanchezs@gmail.com>
2016-08-30 19:41:40 +0000

    Add destroy functionality

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,6 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
+  describe 'grams#destroy' do
+    it 'should allow user to destroy a gram' do
+      p = FactoryGirl.create(:gram)
+      delete :destroy, id: p.id
+      expect(response).to redirect_to root_path
+      p = Gram.find_by_id(p.id)
+      expect(p).to eq nil
+    end
+
+    it 'should return a 404 error message if the gram is not found' do
+      delete :destroy, id: 'HEYHEY'
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
   describe 'grams#update' do
     it 'should allow user to successfully update grams' do
       p = FactoryGirl.create(:gram, message: 'Initial Value')
